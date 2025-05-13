e1afa6097dcf2aecc02313f9f95fd821619e756c
Sanskar Dubey <sanskardubey502@gmail.com>
2025-05-13 23:24:43 +0530

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
     <div>
-      <h1 className='font-bold text-xl'>Home</h1>
-      <div className='flex item-center justify-between'>
+      <h1 className="font-bold text-xl">Home</h1>
+      <div className="flex item-center justify-between">
         <h1>Snippets</h1>
-        <Button>New</Button>
+        <Link href={"/snippet/new"}>
+          <Button>New</Button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
